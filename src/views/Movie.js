314f5ac3633513f12450a1394c205484a009ccc7
wangyubin <18790229321@163.com>
2019-11-27 09:09:02 +0800

    delete react-thunk

--- src/views/Movie.js
+++ src/views/Movie.js
@@ -8,10 +8,10 @@ class Movie extends React.Component{
     render(){
         return (
             <>
-            <nav>
-                <NavLink style={{margin:"30px",color:"skyblue"}} activeStyle={{margin:"30px",color:"red"}} to={'/movie'} exact>正在热映</NavLink>
-                <NavLink style={{margin:"30px",color:"skyblue"}} activeStyle={{margin:"30px",color:"red"}} to={'/movie/coming'}>即将上映</NavLink>
-            </nav>
+				<nav>
+					<NavLink style={{margin:"30px",color:"skyblue"}} activeStyle={{margin:"30px",color:"red"}} to={'/movie'} exact>正在热映</NavLink>
+					<NavLink style={{margin:"30px",color:"skyblue"}} activeStyle={{margin:"30px",color:"red"}} to={'/movie/coming'}>即将上映</NavLink>
+				</nav>
 
                 <Route path={'/movie/coming'} render={()=><GuardRouter component={MovieComing}></GuardRouter>}></Route>
                 <Route path={'/movie'} render={()=><GuardRouter component={MovieOnInfo} ></GuardRouter>} exact></Route>
